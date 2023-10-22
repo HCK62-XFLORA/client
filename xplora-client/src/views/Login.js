bb69045f24de292e3728bd5b3dd1d2d773fd3b20
Akhmadmsm <akhmadmsm@gmail.com>
2023-10-23 06:33:50 +0700

    feat: adding login and add form

--- xplora-client/src/views/Login.js
+++ xplora-client/src/views/Login.js
@@ -1,8 +1,28 @@
 import React from "react";
-import { Button, Image, StyleSheet, Text, View } from "react-native";
-import { useFonts, Karla_500Medium, Karla_600SemiBold, Karla_700Bold } from "@expo-google-fonts/karla";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { TextInput } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  useFonts,
+  Karla_500Medium,
+  Karla_600SemiBold,
+  Karla_700Bold,
+} from "@expo-google-fonts/karla";
 
 const Login = ({ navigation }) => {
+  const [text, setText] = React.useState("");
+
+  const [password, setPassword] = React.useState("");
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
   let [fontsLoaded, fontError] = useFonts({
     Karla_500Medium,
   });
@@ -12,11 +32,63 @@ const Login = ({ navigation }) => {
   }
   return (
     <>
+      <View style={styles.mainContainer}>
+        {/* <Image source={require("../../assets/logo.png")} style={styles.image} /> */}
+        <Text style={styles.header}>Welcome back!</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            activeOutlineColor="#898989"
+            mode="outlined"
+            label="Email"
+            placeholder="Please Input your email"
+            value={text}
+            onChangeText={(text) => setText(text)}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            activeOutlineColor="#898989"
+            mode="outlined"
+            label="Password"
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Enter Password"
+            placeholderTextColor="#aaa"
+            right={
+              <TextInput.Icon
+                icon={showPassword ? "eye-off" : "eye"}
+                color="#aaa"
+                onPress={() => {
+                  setShowPassword(!showPassword);
+                }}
+              />
+            }
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.showPassword}
+          onPress={() => {
+            console.log("forgot password?");
+          }}>
+          <Text> Forgot Password?</Text>
+        </TouchableOpacity>
+        <Button
+          style={styles.buttonSubmit}
+          onPress={() => navigation.navigate("TabNavigator")}
+          title="Submit"
+          color="#06674b"
+          accessibilityLabel="Submit button"
+        />
+      </View>
       <Button
-        onPress={() => navigation.navigate("TabNavigator")}
-        title="home"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
+        style={styles.buttonSubmit}
+        onPress={() => navigation.navigate("AddThread")}
+        title="Add Threads"
+        color="#06674b"
+        accessibilityLabel="Submit button"
       />
     </>
   );
@@ -24,4 +96,47 @@ const Login = ({ navigation }) => {
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    textAlign: "center",
+    fontSize: 21,
+    // color: theme.colors.primary,
+    fontWeight: "bold",
+    paddingVertical: 12,
+    letterSpacing: 0,
+    fontFamily: "Karla_500Medium",
+  },
+  mainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 10,
+  },
+  showPassword: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingTop: -10,
+    paddingBottom: 20,
+  },
+  input: {
+    flex: 1,
+  },
+  buttonSubmit: {
+    alignItems: "center",
+    fontFamily: "Karla_500Medium",
+    marginVertical: 20,
+  },
+  image: {
+    width: 110,
+    height: 110,
+    marginBottom: 8,
+    borderRadius: 30,
+  },
+});
